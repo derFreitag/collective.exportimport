442d9a7249adb90f421c61f29dd9325f8798a47b
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-05-22 18:23:54 +0200

    Back to development: 1.16.post1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ else:
 
 setup(
     name="collective.exportimport",
-    version="1.16.post0",
+    version="1.16.post1.dev0",
     description="An add-on for Plone to Export and import content, members, relations, translations and localroles.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
